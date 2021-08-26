3141aa7744bc262b5089bb768b737bd67b2e96ab
Will Nixon <will_nixon@me.com>
2021-08-26 20:49:31 +0100

    add auth and networking

---
+++ AuthenticationService/AuthenticationService/AuthenticationService.h
@@ -0,0 +1,18 @@
+//
+//  AuthenticationService.h
+//  AuthenticationService
+//
+//  Created by Will Nixon on 8/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthenticationService.
+FOUNDATION_EXPORT double AuthenticationServiceVersionNumber;
+
+//! Project version string for AuthenticationService.
+FOUNDATION_EXPORT const unsigned char AuthenticationServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthenticationService/PublicHeader.h>
+
+
